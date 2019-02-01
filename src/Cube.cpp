19f6ad714ea43854069560e23a52d6f8b3e42202
Misttgun <maurelarchange@gmail.com>
2019-02-01 18:14:25 +0100

    Bug fix intersection cube

--- src/Cube.cpp
+++ src/Cube.cpp
@@ -2,7 +2,7 @@
 
 bool Cube::intersect(const Ray& ray, Vector &impact) const
 {
-    Ray local_ray = local_to_global(ray);
+    /*Ray local_ray = local_to_global(ray);
 
     float t, t1, t2, tnear = -1000.0f, tfar = 1000.0f;
 
@@ -34,45 +34,136 @@ bool Cube::intersect(const Ray& ray, Vector &impact) const
     t = tnear;
 
     impact = local_to_global_point(local_ray.origin + t * local_ray.direction);
-    
+
+    return true;*/
+
+    Vector orig = global_to_local_point(ray.origin);
+    Vector dir = global_to_local_vector(ray.direction).normalized();
+
+    float t;
+
+    float tmin = (-1 - orig[0]) / dir[0];
+    float tmax = (1 - orig[0]) / dir[0];
+
+    if(tmin > tmax)
+        std::swap(tmin, tmax);
+
+    float tymin = (-1 - orig[1]) / dir[1];
+    float tymax = (1 - orig[1]) / dir[1];
+
+    if(tymin > tymax)
+        std::swap(tymin, tymax);
+
+    if((tmin > tymax) || (tymin > tmax))
+        return false;
+
+    if(tymin > tmin)
+        tmin = tymin;    if(tymax < tmax)
+        tmax = tymax;
+
+    float tzmin = (-1 - orig[2]) / dir[2];
+    float tzmax = (1 - orig[2]) / dir[2];
+
+    if(tzmin > tzmax)
+        std::swap(tzmin, tzmax);
+
+    if((tmin > tzmax) || (tzmin > tmax))
+        return false;
+
+    if(tzmin > tmin)
+        tmin = tzmin;
+
+    if(tzmax < tmax)
+        tmax = tzmax;
+
+    t = tmin;
+
+    if(t < 0)
+    {
+        t = tmax;
+        if(t < 0) return false;
+    }
+
+    impact = local_to_global_point(orig) + local_to_global_vector(t * dir);
+
     return true;
 }
 
 Ray Cube::get_normal(const Vector& impact, const Vector& observator) const
 {
-    Vector local_obs = global_to_local_point(observator);
-    Vector local_impact = global_to_local_point(impact);
+    //Vector local_obs = global_to_local_point(observator);
+    //Vector local_impact = global_to_local_point(impact);
 
-    Vector local_ray_direction(local_impact - local_obs);
-    local_ray_direction = local_ray_direction.normalized();
+    //Vector local_ray_direction(local_impact - local_obs);
+    //local_ray_direction = local_ray_direction.normalized();
 
-    // - l'origine se trouve au centre 
-    Vector normal_origin(0, local_impact[1], 0);
-    Vector local_normal(local_impact);
+    //// - l'origine se trouve au centre 
+    //Vector normal_origin(0, local_impact[1], 0);
+    //Vector local_normal(local_impact);
 
-    if (local_impact[0] < -0.9999 && local_impact[0] > -1.0001)
-        local_normal[0] = 2;
+    //if(local_impact[0] < -0.9999 && local_impact[0] > -1.0001)
+    //    local_normal[0] = 2;
 
-    else if (local_impact[0] > 0.9999 && local_impact[0] < 1.0001)
-        local_normal[0] = 0;
+    //else if(local_impact[0] > 0.9999 && local_impact[0] < 1.0001)
+    //    local_normal[0] = 0;
 
-    else if (local_impact[1] < -0.9999 && local_impact[1] > -1.0001)
-        local_normal[1] = 0;
+    //else if(local_impact[1] < -0.9999 && local_impact[1] > -1.0001)
+    //    local_normal[1] = 0;
 
-    else if (local_impact[1] > 0.9999 && local_impact[1] < 1.0001)
-        local_normal[1] = 2;
+    //else if(local_impact[1] > 0.9999 && local_impact[1] < 1.0001)
+    //    local_normal[1] = 2;
 
-    else if (local_impact[2] < -0.9999 && local_impact[2] > -1.0001)
-        local_normal[2] = 0;
+    //else if(local_impact[2] < -0.9999 && local_impact[2] > -1.0001)
+    //    local_normal[2] = 0;
 
-    else if (local_impact[2] > 0.9999 && local_impact[2] < 1.0001)
-        local_normal[2] = 2;
+    //else if(local_impact[2] > 0.9999 && local_impact[2] < 1.0001)
+    //    local_normal[2] = 2;
 
-    Vector local_normal_direction(local_normal - local_impact);
-    local_normal_direction = local_normal_direction.normalized();
+    //Vector local_normal_direction(local_normal - local_impact);
+    //local_normal_direction = local_normal_direction.normalized();
 
-    if (local_ray_direction.dot(local_normal_direction) < 0)
-        return Ray(impact, local_to_global_vector(local_normal_direction).normalized());
+    //if(local_ray_direction.dot(local_normal_direction) < 0)
+    //    return Ray(impact, local_to_global_vector(local_normal_direction).normalized());
 
-    return Ray(impact, local_to_global_vector(local_normal_direction * -1).normalized());
+    //return Ray(impact, local_to_global_vector(local_normal_direction * -1).normalized());
+
+    Vector nImpact = global_to_local_point(impact);
+    Vector nObservator = global_to_local_point(observator);
+    Vector normal(0, 0, 0);
+    Vector dir = nImpact - nObservator;
+    dir = dir.normalized();
+
+    if(std::abs(nImpact[0] - 1.0f) < 0.0001f)
+    {
+        normal = Vector(1, 0, 0);
+    }
+    else if(std::abs(nImpact[1] - 1.0f) < 0.0001f)
+    {
+        normal = Vector(0, 1, 0);
+    }
+    else if(std::abs(nImpact[2] - 1.0f) < 0.0001f)
+    {
+        normal = Vector(0, 0, 1);
+    }
+    else if(std::abs(nImpact[0] + 1.0f) < 0.0001f)
+    {
+        normal = Vector(-1, 0, 0);
+    }
+    else if(std::abs(nImpact[1] + 1.0f) < 0.0001f)
+    {
+        normal = Vector(0, -1, 0);
+    }
+    else if(std::abs(nImpact[2] + 1.0f) < 0.0001f)
+    {
+        normal = Vector(0, 0, -1);
+    }
+
+    auto res = dir.dot(normal);
+    if(res > 0)
+        normal = normal * -1;
+
+    normal = local_to_global_vector(normal);
+    normal = normal.normalized();
+
+    return Ray(impact, normal);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -15,164 +15,166 @@
 #include "Cube.h"
 #include "Cone.h"
 
-int main() {
-	/*
-	HPoint vec(1, 2, 3, 2);
-	HPoint vac(4, 5, 6, 3);
+int main()
+{
+    /*
+    HPoint vec(1, 2, 3, 2);
+    HPoint vac(4, 5, 6, 3);
 
-	HVector res = vac - vec;
-	//float res = vac.dot(vec);
+    HVector res = vac - vec;
+    //float res = vac.dot(vec);
 
-	Point vec1(1, 2, 3);
-	Point vac1(4, 5, 6);
+    Point vec1(1, 2, 3);
+    Point vac1(4, 5, 6);
 
-	Vector res1 = vac1;
+    Vector res1 = vac1;
 
-	Matrix mat;
-	mat(0, 0) = 5;
-	mat(0, 1) = -2;
-	mat(0, 2) = 2;
-	mat(0, 3) = 7;
+    Matrix mat;
+    mat(0, 0) = 5;
+    mat(0, 1) = -2;
+    mat(0, 2) = 2;
+    mat(0, 3) = 7;
 
-	mat(1, 0) = 1;
-	mat(1, 1) = 0;
-	mat(1, 2) = 0;
-	mat(1, 3) = 3;
+    mat(1, 0) = 1;
+    mat(1, 1) = 0;
+    mat(1, 2) = 0;
+    mat(1, 3) = 3;
 
-	mat(2, 0) = -3;
-	mat(2, 1) = 1;
-	mat(2, 2) = 5;
-	mat(2, 3) = 0;
+    mat(2, 0) = -3;
+    mat(2, 1) = 1;
+    mat(2, 2) = 5;
+    mat(2, 3) = 0;
 
-	mat(3, 0) = 3;
-	mat(3, 1) = -1;
-	mat(3, 2) = -9;
-	mat(3, 3) = 4;
+    mat(3, 0) = 3;
+    mat(3, 1) = -1;
+    mat(3, 2) = -9;
+    mat(3, 3) = 4;
 
-	Matrix adj;
-	mat.adjoint(adj);
+    Matrix adj;
+    mat.adjoint(adj);
 
-	Matrix inverse = mat.inverse();
+    Matrix inverse = mat.inverse();
 
-	HVector product = mat * vec;
+    HVector product = mat * vec;
 
 
-	std::cout << "(" << product[0] << ", " << product[1] << ", " << product[2] << ", " << product[3] << ")" << std::endl;
-	std::cout << "-----------------------------------" << std::endl;
-	std::cout << "(" << res.x << ", " << res.y << ", " << res.z << ", " << res.w << ")" << std::endl;
-	std::cout << "-----------------------------------" << std::endl;
-	std::cout << "(" << res1.x << ", " << res1.y << ", " << res1.z << ")" << std::endl;
-	std::cout << "-----------------------------------" << std::endl;
-	mat.display();
-	std::cout << "-----------------------------------" << std::endl;
-	adj.display();
-	std::cout << "-----------------------------------" << std::endl;
-	inverse.display();
-	*/
-	Scene scene;
+    std::cout << "(" << product[0] << ", " << product[1] << ", " << product[2] << ", " << product[3] << ")" << std::endl;
+    std::cout << "-----------------------------------" << std::endl;
+    std::cout << "(" << res.x << ", " << res.y << ", " << res.z << ", " << res.w << ")" << std::endl;
+    std::cout << "-----------------------------------" << std::endl;
+    std::cout << "(" << res1.x << ", " << res1.y << ", " << res1.z << ")" << std::endl;
+    std::cout << "-----------------------------------" << std::endl;
+    mat.display();
+    std::cout << "-----------------------------------" << std::endl;
+    adj.display();
+    std::cout << "-----------------------------------" << std::endl;
+    inverse.display();
+    */
+    Scene scene;
 
-	//scene.load("config.txt");
+    //scene.load("config.txt");
 
-	Material mat_red(Color(1, 0, 0), Color(1, 0, 0), Color(1, 0, 0), 1);
-	Material mat_blue(Color(0, 0, 1), Color(0, 0, 1), Color(0, 0, 1), 1);
+    Material mat_red(Color(1, 0, 0), Color(1, 0, 0), Color(1, 0, 0), 10);
+    Material mat_blue(Color(0, 0, 1), Color(0, 0, 1), Color(0, 0, 1), 5);
     Material mat_green(Color(0, 1, 0), Color(0, 1, 0), Color(0, 1, 0), 1);
     Material mat_grey(Color(0.5, 0.5, 0.5), Color(0.5, 0.5, 0.5), Color(0.5, 0.5, 0.5), 1);
 
-	//std::shared_ptr<Sphere> p = std::make_shared<Sphere>(Sphere());
-	std::shared_ptr<Plan> p = std::make_shared<Plan>(Plan());
-	p->translate(0, 0, 6);
-	p->set_material(mat_grey);
-	scene.add_object(p);
+    //std::shared_ptr<Sphere> p = std::make_shared<Sphere>(Sphere());
+    std::shared_ptr<Plan> p = std::make_shared<Plan>(Plan());
+    p->translate(0, 0, 6);
+    p->set_material(mat_grey);
+    scene.add_object(p);
 
     // FRONT IS BLUE
-	std::shared_ptr<Sphere> p2 = std::make_shared<Sphere>(Sphere());
-	//std::shared_ptr<Square> p2 = std::make_shared<Square>(Square());
-	p2->translate(0, 1, 0);
-	p2->set_material(mat_blue);
-	scene.add_object(p2);
+    //std::shared_ptr<Sphere> p2 = std::make_shared<Sphere>(Sphere());
+    ////std::shared_ptr<Square> p2 = std::make_shared<Square>(Square());
+    //p2->translate(0, 1, 0);
+    //p2->set_material(mat_blue);
+    //scene.add_object(p2);
 
-    // RIGHT IS GREEN
-	std::shared_ptr<Sphere> p3 = std::make_shared<Sphere>(Sphere());
-	p3->translate(1, 0, 2);
-	p3->set_material(mat_green);
-	scene.add_object(p3);
+ //   // RIGHT IS GREEN
+    //std::shared_ptr<Sphere> p3 = std::make_shared<Sphere>(Sphere());
+    //p3->translate(1, 0, 2);
+    //p3->set_material(mat_green);
+    //scene.add_object(p3);
 
     // LEFT IS RED
-	std::shared_ptr<Sphere> p4 = std::make_shared<Sphere>(Sphere());
-	p4->translate(-2, 1, 2);
+    std::shared_ptr<Sphere> p4 = std::make_shared<Sphere>(Sphere());
+    p4->translate(-1, 1, 2);
     p4->scale(2);
-	p4->set_material(mat_red);
-	scene.add_object(p4);
+    p4->set_material(mat_blue);
+    scene.add_object(p4);
 
-    
-    std::shared_ptr<Cylinder> p5 = std::make_shared<Cylinder>(Cylinder());
-    p5->translate(1, -1, 3);
-    //p5->rotate_x(45);
-    p5->scale(2);
-    p5->set_material(mat_green);
-    scene.add_object(p5);
+    //   
+    //   std::shared_ptr<Cylinder> p5 = std::make_shared<Cylinder>(Cylinder());
+    //   p5->translate(1, -1, 3);
+    //   //p5->rotate_x(45);
+    //   p5->scale(2);
+    //   p5->set_material(mat_green);
+    //   scene.add_object(p5);
 
-    std::shared_ptr<Cone> p6 = std::make_shared<Cone>(Cone());
-    p6->translate(0, 0, 0);
-    p6->scale(2);
-    //p5->rotate_x(45);
-    p6->set_material(mat_red);
-    scene.add_object(p6);
+    //   std::shared_ptr<Cone> p6 = std::make_shared<Cone>(Cone());
+    //   p6->translate(0, 0, 0);
+    //   p6->scale(2);
+    //   //p5->rotate_x(45);
+    //   p6->set_material(mat_red);
+    //   scene.add_object(p6);
 
     std::shared_ptr<Cube> p7 = std::make_shared<Cube>(Cube());
-    p7->translate(-1, 1, 0);
-    p7->scale(0.5);
+    p7->scale(2);
+    p7->translate(0, 1, 3);
+
     p7->rotate_x(45);
     p7->rotate_y(45);
     p7->set_material(mat_red);
     scene.add_object(p7);
-    
 
-	std::shared_ptr<Light> l = std::make_shared<Light>(Light(2, 2, 0));
-	l->id = Color(0.5f, 0.5f, 0.5f);
-	l->is = Color(0.7f, 0.7f, 0.7f);
-	scene.add_light(l);
+
+    std::shared_ptr<Light> l = std::make_shared<Light>(Light(2, 1.5, 0));
+    l->id = Color(0.4f, 0.4f, 0.4f);
+    l->is = Color(0.8f, 0.8f, 0.8f);
+    scene.add_light(l);
 
     // SETTING CAMERA
     Camera test(0, 0, -5, 3);
     test.rotate_y(180);
-	scene.set_camera(test);
-	
+    scene.set_camera(test);
+
 
     // SET META DATA
     scene.set_bg(Color(1, 1, 1));
-	scene.set_ambiant(Color(0.5, 0.5, 0.5));
+    scene.set_ambiant(Color(0.5, 0.5, 0.5));
 
-	std::cout << "Scene is loaded with " << scene.nb_objects() << " objects and "
-		<< scene.nb_lights() << " lights\n";
+    std::cout << "Scene is loaded with " << scene.nb_objects() << " objects and "
+        << scene.nb_lights() << " lights\n";
 
 
-	Renderer renderer;
-	Camera cam = scene.get_camera();
+    Renderer renderer;
+    Camera cam = scene.get_camera();
 
     const int size = 800;
 
     Color** render_2 = new Color*[size];
-    for (int i = 0; i < size; i++)
+    for(int i = 0; i < size; i++)
         render_2[i] = new Color[size];
 
     float max = -INFINITY;
 
-	for (int i = 0; i < size; i++)
-	{
-		for (int j = 0; j < size; j++)
-		{
-			float x = static_cast<float>(j) / size, y = static_cast<float>(i) / size;
+    for(int i = 0; i < size; i++)
+    {
+        for(int j = 0; j < size; j++)
+        {
+            float x = static_cast<float>(j) / size, y = static_cast<float>(i) / size;
 
-			Ray ray = cam.get_ray(x, y);
+            Ray ray = cam.get_ray(x, y);
             Vector impact;
-			std::shared_ptr<Object> intersected = scene.closer_intersected(ray, impact);
+            std::shared_ptr<Object> intersected = scene.closer_intersected(ray, impact);
 
-			if (intersected == nullptr)
-			{
-				render_2[i][j] = scene.get_background();
-				continue;
-			}
+            if(intersected == nullptr)
+            {
+                render_2[i][j] = scene.get_background();
+                continue;
+            }
 
             else
             {
@@ -180,39 +182,39 @@ int main() {
                 render_2[i][j] = renderer.get_impact_color(ray, *intersected, impact, scene);
                 max = std::max(std::max(std::max(render_2[i][j].r, render_2[i][j].g), render_2[i][j].b), max);
             }
-		}
-	}
+        }
+    }
 
-	std::ofstream file_ppm;
-	file_ppm.open("render.ppm");
+    std::ofstream file_ppm;
+    file_ppm.open("render.ppm");
 
-	//file_ppm << "P3\n" << width << " " << height << "\n255\n";
+    //file_ppm << "P3\n" << width << " " << height << "\n255\n";
     file_ppm << "P3\n" << size << " " << size << "\n255\n";
 
-	for (int i = 0; i < size; i++)
-	{
-		for (int j = 0; j < size; j++)
-		{
-            if (render_2[i][j].r > 1.f || render_2[i][j].g > 1.f || render_2[i][j].b > 1.f)
+    for(int i = 0; i < size; i++)
+    {
+        for(int j = 0; j < size; j++)
+        {
+            if(render_2[i][j].r > 1.f || render_2[i][j].g > 1.f || render_2[i][j].b > 1.f)
             {
                 render_2[i][j].r /= max;
                 render_2[i][j].g /= max;
                 render_2[i][j].b /= max;
             }
 
-			file_ppm << render_2[i][j].r * 255 << " " << render_2[i][j].g * 255 << " " << render_2[i][j].b * 255 << " ";
-		}
+            file_ppm << render_2[i][j].r * 255 << " " << render_2[i][j].g * 255 << " " << render_2[i][j].b * 255 << " ";
+        }
 
-		file_ppm << "\n";
-	}
-	//std::cout << "Res = " << res << std::endl;
-	std::cout << "DONE !\n";
+        file_ppm << "\n";
+    }
+    //std::cout << "Res = " << res << std::endl;
+    std::cout << "DONE !\n";
 
-    for (auto i = 0; i < size; i++)
+    for(auto i = 0; i < size; i++)
         delete render_2[i];
 
     delete[] render_2;
 
-	getchar();
-	return 0;
+    getchar();
+    return 0;
 }
