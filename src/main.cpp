befbf52bb77dd31f873cc64d73ee979017a145b2
Florian CHAMPAUD <fchampaud@kyriopolis.fr>
2019-02-16 13:50:48 +0100

    Display real time rendering in a SDL window

--- src/main.cpp
+++ src/main.cpp
@@ -23,9 +23,21 @@
 #include <thread>
 #include <atomic>
 #include <future>
+#include <SDL.h>
 
 int main()
 {
+    SDL_Window *window;
+    SDL_Renderer *sdl_renderer;
+    SDL_Texture *texture;
+    SDL_Event event;
+
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,
+                     "Couldn't initialize SDL: %s", SDL_GetError());
+        return 3;
+    }
+
     auto start = std::chrono::steady_clock::now();
 
     std::shared_ptr<Scene> scene = SceneLoader::load("scene.json");
@@ -34,6 +46,15 @@ int main()
 
     std::cout << "Scene is loaded with " << scene->nb_objects() << " objects and "
         << scene->nb_lights() << " lights\n";
+
+    window = SDL_CreateWindow("Ray Tracing",
+                              SDL_WINDOWPOS_UNDEFINED,
+                              SDL_WINDOWPOS_UNDEFINED,
+                              scene_size, scene_size,
+                              0);
+    sdl_renderer = SDL_CreateRenderer(window, -1, 0);
+    texture = SDL_CreateTexture(sdl_renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET,
+                                scene_size, scene_size);
 
 
     Renderer renderer;
@@ -82,6 +103,36 @@ int main()
         }));
     }
 
+    while (true) {
+        Color color;
+        SDL_PollEvent(&event);
+        if (event.type == SDL_QUIT) {
+            break;
+        }
+
+        //SDL_SetRenderTarget(sdl_renderer, texture);
+
+        for (int i = 0; i < scene_size; i++) {
+            for (int j = 0; j < scene_size; j++) {
+                color = scene->image[i * scene->_sampling_factor][j * scene->_sampling_factor];
+                SDL_SetRenderDrawColor(sdl_renderer,
+                                       static_cast<Uint8> (color.r * 255),
+                                       static_cast<Uint8> (color.g * 255),
+                                       static_cast<Uint8> (color.b * 255), 255);
+                SDL_RenderDrawPoint(sdl_renderer, j, i);
+            }
+        }
+
+        //SDL_SetRenderTarget(sdl_renderer, nullptr);
+
+        //SDL_RenderCopy(sdl_renderer, texture, nullptr, nullptr);
+        SDL_RenderPresent(sdl_renderer);
+
+        SDL_Delay(100);
+    }
+
+    std::cout << "Now saving antialiased image..." << std::endl;
+
     for(int i = 0; i < future_vector.size(); i++)
     {
         future_vector.at(i).get();
@@ -110,6 +161,10 @@ int main()
         delete antialiased_image[i];
 
     delete[] antialiased_image;
+
+    SDL_DestroyRenderer(sdl_renderer);
+    SDL_Quit();
+
     //std::cout << "Res = " << res << std::endl;
     std::cout << "DONE !\n";
 
